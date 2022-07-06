28739197538535502e614778df0b7135030b2d71
Dalian <tillierdalian@gmail.com>
2022-07-06 14:40:04 +0200

    ajout

---
+++ html/Exo4/main.js
@@ -0,0 +1,58 @@
+let personnage = [
+  {
+    id: 1,
+    nom: "Itachi",
+    Histoire: `Frère
+    aîné de Sasuke Uchiwa et excellent ninja de Konoha, il décima son
+    clan pour défendre Konoha contre la rébellion des Uchiwa qui
+    voulaient faire un coup d'état pour prendre le pouvoir.`,
+  },
+
+  {
+    id: 2,
+    nom: "Sasuke",
+    Histoire: `Petit frère d'Itachi c'est un ninja du village
+  de Konoha au caractère calme et détaché, conçu pour être à la fois
+  le partenaire et le rival du personnage principal, Naruto Uzumaki.`,
+  },
+  {
+    id: 3,
+    nom: "Madara",
+    Histoire: `Ancien leader du clan Uchiwa du temps de la fondation de Konoha. Il
+  était l'ami d'enfance de Hashirama Senju. Son but est de rassembler
+  les neuf bijū pour recréer un monstre ancestral appelé Jûbi.`,
+  },
+  {
+    id: 4,
+    nom: "Kakashi",
+    Histoire: `Dans l'univers de la série, Kakashi est un ninja
+  d’élite du village de Konoha.`,
+  },
+  {
+    id: 5,
+    nom: "Naruto",
+    Histoire: `Dans
+  l'univers de la série, Naruto est un jeune ninja du village de
+  Konoha. Hôte du démon renard à neuf queues, une créature qui a
+  attaqué le village par le passé, il est rejeté par les autres
+  villageois.`,
+  },
+  {
+    id: 6,
+    nom: "Shikamaru",
+    Histoire: `C'est un ninja du
+  village de Konoha, Shikamaru est le 16ᵉ chef du Clan Nara. Il est
+  l'un des meilleurs et plus fidèles amis de Naruto.`,
+  },
+  {
+    id: 7,
+    nom: "Pain",
+    Histoire: `Il est celui
+  qui contrôle Pain ; bien que ce soit lui qui s'exprime à travers les
+  6 corps, Nagato n'est pas Pain, car ce nom ne désigne que les six
+  cadavres que Nagato manipule dans l'ombre. Il siège généralement
+  dans le village caché d'Ame qu'il dirige à travers Pain.`,
+  },
+];
+
+console.log(personnage);
